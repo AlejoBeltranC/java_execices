92d25453ca8111d0573f52fbc2075308328cef9f
AlejoBeltranC <85804179+AlejoBeltranC@users.noreply.github.com>
2021-11-05 10:52:58 -0500

    Update Credentials.java

--- PropuestaReto5/CRUD_JAVA/Reto_5/src/Model/Credentials.java
+++ PropuestaReto5/CRUD_JAVA/Reto_5/src/Model/Credentials.java
@@ -7,8 +7,8 @@ package Model;
  */
 public interface Credentials {
     
-    String URL      = "jdbc:mysql://127.0.0.1:3306/Contenidos?serverTimezone=UTC";
-    String USER     = "root";
-    String PASSWORD = "3057440958A";
+    String URL      = "";
+    String USER     = "";
+    String PASSWORD = "";
     
 }
